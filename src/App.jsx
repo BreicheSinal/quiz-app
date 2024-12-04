82ca89a0a233a054926323ba328e85c6fe26ad11
BreicheSinal <sinalbreiche@gmail.com>
2024-12-04 22:24:45 +0200

    Imported the store into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Provider } from "react-redux";
 import "./styles/App.css";
 import "./styles/index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import store from "./redux/store";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <Provider>
+        <Provider store={store}>
           <Routes></Routes>
         </Provider>
       </BrowserRouter>
