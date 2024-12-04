cc6c7591a0e1824b331b7369f55204aa78f12615
BreicheSinal <sinalbreiche@gmail.com>
2024-12-05 01:59:02 +0200

    Added my routes for each page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { Provider } from "react-redux";
-import "./styles/App.css";
-import "./styles/index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import store from "./redux/store";
+
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+
+import "./styles/App.css";
+import "./styles/index.css";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Provider store={store}>
-          <Routes></Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quiz" element={<Quiz />} />
+          </Routes>
         </Provider>
       </BrowserRouter>
     </div>
