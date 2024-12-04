fad70896fb72deda35b843ec20afbb6a44775f7d
BreicheSinal <sinalbreiche@gmail.com>
2024-12-05 01:33:46 +0200

    Imported the slice inside the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import quizReducer from "./users/quizSlice";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    quiz: quizReducer,
+  },
 });
 
 export default store;
