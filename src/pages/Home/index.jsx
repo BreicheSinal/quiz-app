e7b06f7c5628d3fd62174ca2a290cfa2daea1b7d
BreicheSinal <sinalbreiche@gmail.com>
2024-12-05 02:58:39 +0200

    Added the score at home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./style.css";
 
 const Home = () => {
   const navigate = useNavigate();
+  const score = useSelector((state) => state.quizState.score);
+
+  console.log({ score });
 
   const navigateToQuiz = () => {
     navigate("/quiz");
@@ -13,7 +17,7 @@ const Home = () => {
   return (
     <div>
       <h1>WELCOME MATE!</h1>
-      <p>Score:</p>
+      <p>Score:{score}</p>
       <button onClick={navigateToQuiz}>Start</button>
     </div>
   );
--- src/redux/users/quizSlice.js
+++ src/redux/users/quizSlice.js
@@ -1,12 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  initialState: {
-    questionIndex: 0,
-    selectedAns: [],
-    score: 0,
-    data: [],
-  },
+  questionIndex: 0,
+  selectedAns: [],
+  score: 0,
+  data: [],
 };
 
 const quizSlice = createSlice({
