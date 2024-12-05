739ed1ec1bead465a74285f5c446563f16214ac8
BreicheSinal <sinalbreiche@gmail.com>
2024-12-05 04:09:43 +0200

    Added validation

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
 const Quiz = () => {
   const { quizIndex } = useParams();
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const quizData = useSelector((state) => state.quizState.data);
   const qsIndex = useSelector((state) => state.quizState.questionIndex);
   const score = useSelector((state) => state.quizState.score);
   const selectedAns = useSelector((state) => state.quizState.selectedAns);
+
+  if (!quizData || !quizData[quizIndex]) navigate("/");
 
   return <div></div>;
 };
