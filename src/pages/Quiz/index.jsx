9d9bfe8f6b69fe712be1584fb1b67070560bcf31
BreicheSinal <sinalbreiche@gmail.com>
2024-12-05 04:06:21 +0200

    Got quiz data from redux store

--- src/pages/Quiz/index.jsx
+++ src/pages/Quiz/index.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 const Quiz = () => {
   const { quizIndex } = useParams();
+
+  const dispatch = useDispatch();
+
+  const quizData = useSelector((state) => state.quizState.data);
+  const qsIndex = useSelector((state) => state.quizState.questionIndex);
+  const score = useSelector((state) => state.quizState.score);
+  const selectedAns = useSelector((state) => state.quizState.selectedAns);
 
   return <div></div>;
 };
