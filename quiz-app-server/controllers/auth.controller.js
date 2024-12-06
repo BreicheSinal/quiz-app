0577755f5371ad1ccc0238c0a4abcc21d5174bf3
BreicheSinal <sinalbreiche@gmail.com>
2024-12-06 20:01:34 +0200

    Added register function | make use of my error utility

--- quiz-app-server/controllers/auth.controller.js
+++ quiz-app-server/controllers/auth.controller.js
@@ -1,7 +1,36 @@
 import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { throwError, throwNotFound } from "../utils/error.js";
 
 export const register = async (req, res) => {
   const { email, username, password } = req.body;
+  try {
+    if (!username || !password) {
+      return throwError({
+        message: "Username and password are required",
+        res,
+        status: 400,
+      });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      email,
+      username,
+      password: hashed,
+    });
+
+    return res.json(user);
+  } catch (error) {
+    console.log("Error in registration:", error.message);
+
+    return throwError({
+      message: "Registration failed",
+      res,
+      status: 500,
+    });
+  }
 };
