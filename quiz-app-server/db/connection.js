8fe8cf0efc74146cc43b8cbf62bfe73066d02d8f
BreicheSinal <sinalbreiche@gmail.com>
2024-12-06 18:01:52 +0200

    Connected to db successfully

---
+++ quiz-app-server/db/connection.js
@@ -0,0 +1,12 @@
+import { connect } from "mongoose";
+
+const connectToDatabase = async () => {
+  try {
+    await connect("mongodb://localhost:27017/quizAppdb");
+    console.log("Connected to db!");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectToDatabase;
--- quiz-app-server/index.js
+++ quiz-app-server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
-
+import connectToDatabase from "./db/connection.js";
 const app = express();
 
 app.listen(8080, async () => {
   console.log("Server running on port 8080 :)");
+
+  connectToDatabase();
 });
