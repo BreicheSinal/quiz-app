47d814476d30c1cae637ffbfc370f915fc5ab681
BreicheSinal <sinalbreiche@gmail.com>
2024-12-06 17:37:05 +0200

    Created user schema

---
+++ quiz-app-server/routes/user.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const User = model("User", userSchema);
