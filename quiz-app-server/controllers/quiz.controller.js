74d65ef8f84e0c6d49612d889c8019c0b0d2540a
BreicheSinal <sinalbreiche@gmail.com>
2024-12-09 18:03:18 +0200

    Added method to fetch data of quizzes

--- quiz-app-server/controllers/quiz.controller.js
+++ quiz-app-server/controllers/quiz.controller.js
@@ -14,3 +14,12 @@ export const seedQuizzes = async (req, res) => {
       .send({ message: "Error seeding quizzes", error: error.message });
   }
 };
+
+export const getQuizzes = async (req, res) => {
+  try {
+    const quizzes = await Quiz.find();
+    res.status(200).json(quizzes);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching quiz data", error });
+  }
+};
