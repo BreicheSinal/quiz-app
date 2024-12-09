fc83157fa5cebd4f7f6a3b6d7f0a340fb18667fd
BreicheSinal <sinalbreiche@gmail.com>
2024-12-09 19:30:50 +0200

    Updated Home page

--- FE/src/pages/Home/index.jsx
+++ FE/src/pages/Home/index.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { loadQuizzes } from "../../redux/thunks/quizThunk.js";
 
 import "./style.css";
 
 const Home = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const totalScore = useSelector((state) => state.quizState.totalScore);
   const quizData = useSelector((state) => state.quizState.data);
+
+  useEffect(() => {
+    dispatch(loadQuizzes());
+  }, [dispatch]);
 
   const navigateToQuiz = (index) => {
     navigate(`/quiz/${index}`, { state: { quizIndex: index } });
