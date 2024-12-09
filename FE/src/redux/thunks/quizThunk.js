dd9331684b9af0d0d380951d3a70d9178eeddafc
BreicheSinal <sinalbreiche@gmail.com>
2024-12-09 19:39:40 +0200

    Added the auth middleware to acces home page

--- FE/src/redux/thunks/quizThunk.js
+++ FE/src/redux/thunks/quizThunk.js
@@ -3,17 +3,20 @@ import { useNavigate } from "react-router-dom";
 
 export const loadQuizzes = () => async (dispatch) => {
   const navigate = useNavigate();
+  const token = localStorage.getItem("token");
 
   try {
     const response = await fetch("http://localhost:8080/quizzes", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
     });
 
     if (!response.ok) {
       navigate("/login");
+      return;
     }
 
     const data = await response.json();
